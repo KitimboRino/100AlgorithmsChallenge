331057e6913d6b752e3b6f7970b12aea819d7eaa
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-06 14:23:15 +0300

    add

--- add/add.ts
+++ add/add.ts
@@ -3,6 +3,10 @@ export function add(param1: number, param2: number): number {
   return param1 + param2;
 }
 
+console.log(add(1, 2));
+console.log(add(3, 2));
+
+
 // Write a function that returns the sum of all numbers regardless of # of params.
 export function add2(...param1: number[]): number {
   let total = 0;
@@ -12,9 +16,6 @@ export function add2(...param1: number[]): number {
   });
   return total;
 }
-
-console.log(add(1, 2));
-console.log(add(3, 2));
 
 console.log(add2(1, 2, 3, 4, 5));
 console.log(add2(2, 3));
