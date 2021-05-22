8bef0f68b43e503846334f564e5a43039cedcb95
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-22 11:39:44 +0300

    Are equally strong algorithm

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,13 @@
 export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft <= yourRight ? yourRight : yourLeft;
 
+  const friendsWeakest = friendsLeft <= friendsRight ? friendsRight : friendsRight;
+  const friendsStrongest = friendsLeft <= friendsRight ? friendsRight : friendsLeft;
+
+  return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))
