37c2cb4af0764469d5aafe6c57bfaac5b570659a
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-22 11:24:06 +0300

    Alternating Sums algorithm

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 export function alternatingSums(a: number[]): number[] {
+  let evenSum = 0;
+  let oddSum = 0;
 
+  //forEach() calls a provided callbackFn function once for each element in an array in ascending index order
+  a.forEach((value, index) => {
+    if (index % 2 == 0) {
+      evenSum += value;
+    } else {
+      oddSum += value;
+    }
+  });
+  return [evenSum, oddSum];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
