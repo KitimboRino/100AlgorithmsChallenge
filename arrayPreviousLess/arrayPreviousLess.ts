6b14be2e307c0e4a382efb712537b0eafda68308
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-22 15:46:50 +0300

    Array PreviuosLess Algortihm

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,13 +1,13 @@
 export function arrayPreviousLess(items: number[]): number[] {
   const lessThanList = [];
 
-  for (let i = items.length - 1; i >=0; i--){
-    for(let j=i; j>=0; j--){
-      if(items[i] > items[j] {
+  for (let i = items.length - 1; i >= 0; i--) {
+    for (let j = i; j >= 0; j--) {
+      if (items[i] > items[j]) {
         lessThanList.unshift(items[j]);
         break;
-      }else if(j === 0) {
-        lessThanList.unshift(-1)
+      } else if (j === 0) {
+        lessThanList.unshift(-1);
       }
     }
   }
