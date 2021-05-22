cc377b685864308006115d95fde0ae982fb5e19d
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-22 15:52:21 +0300

    Array Replace Algorithm

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,16 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  
+  inputArray.forEach((ele, index) => {
+    if (ele === elemToReplace) {
+      inputArray[index] = substitutionElem;
+    }
+  });
 
+  return inputArray;
 }
 
-// console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 1], 1, 3));
