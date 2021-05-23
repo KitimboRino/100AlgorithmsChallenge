3ec1a8f73b2058185b21a9612357e559edb17b79
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-23 10:07:25 +0300

    Avoid Obstales Algorithm

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,20 @@
 export function avoidObstacles(inputArray: number[]): number {
+  inputArray = inputArray.sort((a, b) => {
+    return a - b;
+  });
 
+  const largestArrayVal = inputArray[inputArray.length - 1];
+
+  for (let index = 1; index <= largestArrayVal + 1; index++) {
+    if (inputArray.every((element) => element % index !== 0)) {
+      return index;
+    }
+  }
 }
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+
+// Test
+// test('avoid obstacles', ()=>{
+//   expect(avoidObstacles([5,3,6,7,9]))
+// })
