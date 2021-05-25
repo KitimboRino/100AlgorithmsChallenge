488ada8119c0e0f83678ef425206ae4a6b629f7e
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-25 07:20:01 +0300

    Candies algorithm

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,9 @@
 export function candies(n: number, m: number): number {
+  // Math.floor() returns the largest integer less than or equal to a given number.
 
+  const candy = Math.floor(m / 3);
+
+  return candy * n;
 }
 
-// console.log(candies(3, 10));
+console.log(candies(3, 10));
