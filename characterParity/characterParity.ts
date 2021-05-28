d513eb043f00e1be01e1a6fa2f6751f0d3887079
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-28 07:28:37 +0300

    Character Parity Algorithm

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,11 @@
 export function characterParity(symbol: string): string {
+  // parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN.
+  const result = parseInt(symbol);
 
+  // ? is an alternative for an if statement.
+  return isNaN(result) ? 'not a number' : result % 2 === 0 ? 'Even' : 'odd';
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
