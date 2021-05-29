55d75ae4de73e4026ae6ebc2a372da04f9b20e3c
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-05-29 08:42:53 +0300

    Check palindrome algorithm

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,18 @@
 export function checkPalindrome(inputString: string): boolean {
+  let reversed = inputString.toLowerCase().split('').reverse().join('');
 
+  return reversed === inputString;
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome('aabaa'));
+console.log(checkPalindrome('abac'));
+console.log(checkPalindrome('a'));
+
+
+
+
+// Test
+// test('Case insesntive Palindrome', () => {
+//   expect(checkPalindrome('AaBaa')).toEqual(true);
+//   expect(checkPalindrome('abac')).toEqual(false);
+// });
