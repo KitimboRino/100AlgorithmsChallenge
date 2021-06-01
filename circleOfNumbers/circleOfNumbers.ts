312bb6fb1768bbbeea28b1293eafe2088b2b90f5
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-06-01 09:29:59 +0300

    circle of Numbers algorithm

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,22 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
+  const circle = [];
+  const halfWay = n / 2;
 
+  for (let index = 0; index < n; index++) {
+    // The push() method adds new items to the end of an array, and returns the new length.
+    circle.push(index);
+  }
+
+  if (firstNumber < halfWay) {
+    return circle[firstNumber + halfWay];
+  }
+
+  return circle[firstNumber - halfWay];
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 2));
+
+// Test
+// test('circle Of Numbers', ()=>{
+//   expect(circleOfNumbers(10,2)).toEqual(7)
+// })
