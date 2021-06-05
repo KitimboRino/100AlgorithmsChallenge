e736b0d02d8138e575ffd6e0c904d35f68a49107
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-06-05 11:37:51 +0300

    Compare Integers Algorithm

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,16 @@
 export function compareIntegers(a: string, b: string): string {
-
+  return parseInt(a) > parseInt(b)
+    ? 'greater' : parseInt(a) < parseInt(b)
+      ? 'less' : 'equal';
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
+
+// Test
+// test('compare Integers',() => {
+// expect('compareIntegers('12','13')).toEqual('less')
+// expect('compareIntegers('875','799')).toEqual('greater')
+// expect('compareIntegers('1000','1000')).toEqual('equal')
+// });
