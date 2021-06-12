90f3996c543450890164c40f2d56da0e4b0519cb
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-06-13 00:17:55 +0300

    Conatains Close Numbers Algorithm

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,6 +1,20 @@
 export function containsCloseNums(nums: number[], k: number): boolean {
-
+  
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i !== j && nums[i] === nums[j]) {
+        return Math.abs(i - j) <= k;
+      }
+    }
+  }
+  return false;
 }
 
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
-// console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
+console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 2));
+
+// Test
+// test('contains Close Nums', () => {
+//   expect(containsCloseNums([0, 1, 2, 3, 5, 2], 3)).toEqual(true);
+//   expect(containsCloseNums([0, 1, 2, 3, 5, 2], 2)).toEqual(true);
+// });
