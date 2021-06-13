077414e4727008d9b999b4faa6a37272cbce3003
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-06-13 09:53:13 +0300

    Contains Duplicates Algorithm

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -3,6 +3,8 @@ export function containsCloseNums(nums: number[], k: number): boolean {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length; j++) {
       if (i !== j && nums[i] === nums[j]) {
+
+        // 
         return Math.abs(i - j) <= k;
       }
     }
--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,19 @@
 export function containsDuplicates(a: number[]): boolean {
+  a = a.sort((a, b) => a - b);
 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === a[i + 1]) {
+      return true;
+    }
+  }
+  return false;
 }
 
-// console.log(containsDuplicates([1, 2, 3, 1]));
-// console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1]));
+
+// Test
+// test('contains Duplicates', () => {
+//   expect(containsDuplicates([1, 2, 3, 1]).toEqual(true));
+//   expect(containsDuplicates([3, 1]).toEqual(false));
+// });
