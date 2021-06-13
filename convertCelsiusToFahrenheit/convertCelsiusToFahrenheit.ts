530a2d1634a4ce0b4868953de8b9f3f6d949dd2f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2021-06-13 10:01:58 +0300

    Convert Celsius To Farenheight Algorithm

--- convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
+++ convertCelsiusToFahrenheit/convertCelsiusToFahrenheit.ts
@@ -1,7 +1,14 @@
 export function celsiusToFahrenheit(celsius: number): number {
-
+  return (celsius * (9/5)) + 32;
 }
 
-// console.log(celsiusToFahrenheit(-30));
-// console.log(celsiusToFahrenheit(-10));
-// console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(-30));
+console.log(celsiusToFahrenheit(-10));
+console.log(celsiusToFahrenheit(0));
+
+// Test
+// test('convert Celcius r=to Fahrenheight', () => {
+//   expect(convertToFahrenheit(-30)).toEqual(-22);
+//   expect(convertToFahrenheit(-10)).toEqual(14);
+//   expect(convertToFahrenheit(0)).toEqual(32);
+// });
