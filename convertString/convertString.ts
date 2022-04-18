f4b26f5ebac33dce7dbc5e8d10100311ef8da365
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-04-18 14:29:14 +0300

    convert to string

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,6 +1,21 @@
 export function convertString(s: string, t: string): boolean {
+    let word = '';
+    let tindex = 0;
+    const sChars = s.split(' ');
 
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
+            if (word === t[tindex]) {
+                word = word.concat(s[i]);
+                tindex++;
+                if(word === t){
+                    return true;
+                }
+            }   
+        }
+    }
+    return false;
 }
 
-// console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
-// console.log(convertString('addbyca', 'abcd'));
+console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
+console.log(convertString('addbyca', 'abcd'));
