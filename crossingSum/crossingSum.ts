6b1a6c176e83d697c87242f66237f9fba9da6ece
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-04-19 07:34:50 +0300

    Crossing Sum Algorithm

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,20 @@
 export function crossingSum(matrix: number[][], a: number, b: number): number {
-
+    let rowSum = matrix[a].reduce((a, b) => a + b)
+    let columnSum = 0
+    for (let index = 0; index < matrix.length;
+        index++) {
+        columnSum += index !== a ? matrix[index][b] : 0
+    }
+    return rowSum + columnSum
 }
 
-// console.log(crossingSum([[1, 1, 1, 1], 
-//     [2, 2, 2, 2], 
-//     [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1],
+[2, 2, 2, 2],
+[3, 3, 3, 3]], 1, 3));
+
+// Test
+// test('crossing Sum', () =>{
+//     expect(crossingSum([[1, 1, 1, 1],
+//         [2, 2, 2, 2],
+//         [3, 3, 3, 3]], 1, 3)).toEqual(12)
+// });
