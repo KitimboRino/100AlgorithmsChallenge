70cbd1cd22d2072bbdb1b422572837362ec9b0f5
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-04-25 11:22:36 +0300

    Extract each kth algorihtm

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -4,5 +4,11 @@ export function evenDigitsOnly(n: number): boolean {
     return digits.every(digit => parseInt(digit) % 2 === 0);
 }
 
+// Test
+// test('even Digits Only', () => {
+//     expect(evenDigitsOnly(248622)).toEqual(true)
+//     expect(evenDigitsOnly(642386)).toEqual(false)
+//   });
+
 console.log(evenDigitsOnly(248622));
 console.log(evenDigitsOnly(642386));
--- extractEachKth/extractEachKth.ts
+++ extractEachKth/extractEachKth.ts
@@ -1,4 +1,10 @@
 export function extractEachKth(inputArray: number[], k: number): number[] {
+    return inputArray.filter((ele, index) => {
+        
+        return (index + 1) % k !== 0;
+        // Alternatively:
+        // return index % k !== k - 1;
+    })
 }
 
-// console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
