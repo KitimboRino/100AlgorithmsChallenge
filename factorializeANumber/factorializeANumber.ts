9cad3545743d0a6686eb205a8da60b11e87dfce9
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-04-26 09:43:50 +0300

    Factorialize a number algorithm

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,24 @@
 export function factorializeANumber(num: number): number {
+    let total = 1;
 
+    // Loop through the number
+    // for (let i = 1; i <= num; i++) {
+    //     total *= i;
+    // }
+
+    // return total;
+
+    // Alternatively
+    if(num === 1) {
+        return 1;
+    }
+    return num * factorializeANumber(num - 1);
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
+
+// Test
+// test('factorialize A Number',() => {
+//     expect(factorialANumber(5)).toBe(120)
+// })
