3ae4cc5eaab6fe68278a139db24d86ce705b614c
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-04-30 09:18:57 +0300

    Fancy Rides Algorithm

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,21 @@
 export function fancyRide(l: number, fares: number[]): string {
+     const rides = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
 
+        for (let i = fares.length - 1; i >= 0; i--) {
+            // if (l >= fares[i]) {
+            //     return rides[i];
+            // }
+
+            // Alternatively
+            // if(fares[i] <= l) {
+            //     return rides[i];
+            // }
+
+            // Alternatively
+            if (fares[i] * 1 <= 20) {
+                return rides[i];
+            }
+        }
 }
 
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
