24b978347ba712923fb1b5b11b51527baf49aed0
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-05-01 12:04:02 +0300

    Fermactor algorithm

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,21 +1,26 @@
 export function fancyRide(l: number, fares: number[]): string {
-     const rides = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
+    const rides = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
 
-        for (let i = fares.length - 1; i >= 0; i--) {
-            // if (l >= fares[i]) {
-            //     return rides[i];
-            // }
+    for (let i = fares.length - 1; i >= 0; i--) {
+        // if (l >= fares[i]) {
+        //     return rides[i];
+        // }
 
-            // Alternatively
-            // if(fares[i] <= l) {
-            //     return rides[i];
-            // }
+        // Alternatively
+        // if(fares[i] <= l) {
+        //     return rides[i];
+        // }
 
-            // Alternatively
-            if (fares[i] * 1 <= 20) {
-                return rides[i];
-            }
+        // Alternatively
+        if (fares[i] * 1 <= 20) {
+            return rides[i];
         }
+    }
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+
+// test
+// test('fancy Ride', () => {
+//     expect(fancyRide([0.3, 0.5, 0.7, 1, 1.3], 30)).toEqual('UberXL')
+// }); 
--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,17 @@
 export function fermactor(n: number): number[] {
-
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < i; j++) {
+            const sum = Math.pow(i, 2) - Math.pow(j, 2);
+            if (n === sum) {
+                return [i, j]
+            }
+        }
+    }
 }
 
-// console.log(fermactor(15));
+console.log(fermactor(15));
+
+// Test
+// test('fermafactor', () => {
+//     expect(fermafactor(15)).toEqual([4,1])
+//   });
