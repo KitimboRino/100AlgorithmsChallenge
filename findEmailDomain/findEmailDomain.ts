8c297045b265fb9ebd96428a27378e0cf0c3ba8c
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-05-03 09:19:31 +0300

    Find email domain algorithm

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,15 @@
 export function findEmailDomain(address: string): string {
+    // const lastIndex = address.lastIndexOf('@');
+    // return address.slice(lastIndex + 1, address.length);
 
+    // Alternatively (Best)
+    return address.split('@').pop();
 }
 
-// console.log(findEmailDomain('prettyandsimple@example.com'));
-// console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain('prettyandsimple@example.com'));
+console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+
+// Test
+// test('find Emai Domain', () => {
+//     expect(findEmailDomain('<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org')).toEqual('example.org')
+//   });
