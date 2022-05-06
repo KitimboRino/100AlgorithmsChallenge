f76f910a77b691fc1a5a15d90dafb506c61a7428
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-05-06 12:41:04 +0300

    Growing plant Algorithm

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,19 @@
 export function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+    let days = 0;
+    let totalHeight = 0;
 
+    /* A while loop that is checking if the total height is less than the desired height. If it is, it will
+    add one to the days and add the upSpeed to the totalHeight. If the totalHeight is greater than or
+    equal to the desiredHeight, it will subtract the downSpeed from the totalHeight. */
+    while (totalHeight < desiredHeight) {
+        days++;
+        totalHeight += upSpeed;
+
+        if (totalHeight >= desiredHeight) {
+            totalHeight -= downSpeed;
+        }
+    }
+    return days;
 }
 
-// console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910))
