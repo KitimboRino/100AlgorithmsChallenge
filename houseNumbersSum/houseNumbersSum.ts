773289949ba5b8ad3ad2289de5ff92d1ec1fe527
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-05-09 10:13:41 +0300

    Algo: House number sum

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,16 @@
 export function houseNumbersSum(inputArray: number[]): number {
+    let total = 0;
 
+    /* A for loop that is iterating through the array. If the value is 0, it will return the total. If
+    the value is not 0, it will add the value to the total. */
+
+    // When 0 is spotted, the program will stop iterating.
+    for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] === 0) {
+            return total;
+        }
+        total += inputArray[i];
+    }
 }
 
-// console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
