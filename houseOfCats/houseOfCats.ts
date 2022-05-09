318e908411dfb5b88ca82511aadb8ec972b3213f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-05-09 10:23:08 +0300

    Algo: House of cats

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,20 @@
 export function houseOfCats(legs: number): number[] {
+    const peopleCount = [];
 
+  
+    /* A special case for when there is only one person. */
+    if (legs === 2) {
+        return [1]
+    }
+
+    /* Looping through the number of legs and adding the number of people to the array. */
+    while (legs >= 0) {
+        peopleCount.unshift(legs / 2);
+        legs -= 4;
+    }
+
+    return peopleCount;
 }
 
-// console.log(houseOfCats(6));
-// console.log(houseOfCats(2));
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
