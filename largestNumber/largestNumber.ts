e364c6696ad0b0503d4b0af140a9ada5f1ff9540
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-08-12 08:44:01 +0300

    Algo: largestNumber

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,6 @@
 export function largestNumber(n: number): number {
-
+    /* Taking the number 10 to the power of n and subtracting 1 from it. */
+    return Math.pow(10, n) - 1;
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(2));
