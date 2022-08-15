00fecef98b598797701bf020e538e2239e85b6cc
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-08-15 07:54:16 +0300

    Algo: makeArrayConsecutive2

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,19 @@
 export function makeArrayConsecutive2(statues: number[]): number {
 
+    /* Sorting the array in ascending order. */
+    let statuesArray = statues.sort((a, b) => a - b);
+
+    let statuesCount = 0;
+    let statuesDifference = 0;
+    
+   /* Looping through the array and subtracting the current number from the next number. Then it is
+   subtracting 1 from the difference. */
+    for (let i = 0; i < statuesArray.length - 1; i++) {
+        statuesDifference = statuesArray[i + 1] - statuesArray[i];
+        statuesCount += statuesDifference - 1;
+    }
+    
+    return statuesCount;
 }
 
-// console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([6, 2, 3, 8]));
