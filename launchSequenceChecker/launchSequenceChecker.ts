5feb46ed96771ce48bb2cc883454ecc3da8e7582
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-08-15 07:50:16 +0300

    Algo: longestDigitsPrefix

--- launchSequenceChecker/launchSequenceChecker.ts
+++ launchSequenceChecker/launchSequenceChecker.ts
@@ -1,6 +1,16 @@
 export function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
-
+    let systemIndex = 0;
+    let stepIndex = 0;
+    while (systemIndex < systemNames.length && stepIndex < stepNumbers.length) {
+        if (systemNames[systemIndex] === "launchSequenceChecker") {
+            systemIndex++;
+            stepIndex++;
+        } else {
+            systemIndex++;
+        }
+    }
+    return stepIndex === stepNumbers.length;
 }
 
-// console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
-// console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
+console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
+console.log(launchSequenceChecker(["stage_1", "stage_1", "stage_2", "dragon"], [2, 1, 12, 111]));
--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,18 @@
 export function longestDigitsPrefix(inputString: string): string {
+    let longest = "";
 
+    /* Looping through the inputString. */
+    for (let i = 0; i < inputString.length; i++) {
+        
+        /* Checking if the character is a number and if it is, it adds it to the longest string. If it
+        is not a number, it breaks out of the loop. */
+        if (inputString[i] >= "0" && inputString[i] <= "9") {
+        longest += inputString[i];
+        } else {
+        break;
+        }
+    }
+    return longest;
 }
 
-// console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('123aa1'));
