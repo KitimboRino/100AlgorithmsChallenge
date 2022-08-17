713a0ede6874fee74c9d698cfb3f6375cfdffeba
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-08-17 09:03:50 +0300

    Algo(maxMultiple.ts): solve maxMultiple

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,15 @@
 export function maxMultiple(divisor: number, bound: number): number {
+    let maxMultiple = 0;
 
+   /* Looping through the numbers from 1 to the bound and checking if the number is divisible by the
+   divisor. If it is, it sets the maxMultiple to that number. */
+    for (let i = 1; i <= bound; i++) {
+        if (i % divisor === 0) {
+            maxMultiple = i;
+        }
+    }
+    
+    return maxMultiple;
 }
 
-// console.log(maxMultiple(3,10));
+console.log(maxMultiple(3,10));
