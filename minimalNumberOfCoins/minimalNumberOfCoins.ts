166f9fb4147f9bd46d85e27337e6f11c5d70be4d
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-08-26 09:52:34 +0300

    Algo(minimalNumberOfCoins.ts): solve minimalNumberOfCoins

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,19 @@
 export function minimalNumberOfCoins(coins: number[], price: number): number {
+    let costLeft = price;
 
+    // Looping through every coin in the array and
+    let total = 0;
+    for (let coin = coins.length - 1; coin >= 0; coin--) {
+        
+        // Find the number of this coin value that can be used, and then update the leftover price to be the remainder.
+        total += Math.floor(costLeft / coins[coin]);
+        costLeft %= coins[coin];
+
+        // If this total is equal to the price, return the total. 
+        if (total === price)
+            return total
+    }
+    return total
 }
 
-// console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([1, 2, 10], 28));
