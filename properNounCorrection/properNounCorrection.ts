64a4fac7ccc52c7de7efce451a07e8d4844067d4
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-09-11 21:33:53 +0300

    Algo(properNounCorrection): Solve properNounCorrection

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,9 @@
-export function properNounCorrection(noun: string): string {
+//Proper nouns always begin with a capital letter, followed by small letters.
 
+//Correct a given proper noun so that it fits this statement.
+export function properNounCorrection(noun: string): string {
+    return noun[0].toUpperCase() + noun.slice(1).toLowerCase();
 }
 
-// console.log(properNounCorrection('pARiS'));
-// console.log(properNounCorrection('John'));
+console.log(properNounCorrection('pARiS'));
+console.log(properNounCorrection('John'));
