cf37859325d0bdd80662c3f6d69d383445b77b29
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-09-11 21:14:28 +0300

    Algo(palindromeRearranging): Solve palindromeRearranging

--- palindromeRearranging/palindromeRearranging.ts
+++ palindromeRearranging/palindromeRearranging.ts
@@ -1,5 +1,29 @@
-export function palindromeRearranging(inputString: string): boolean {
+/*
+Given a string, find out if its characters can be rearranged to form a palindrome.
+*/
 
+export function palindromeRearranging(inputString: string): boolean {
+    //Create a character map in an object for the string characters.
+    let charMap = {}
+    for (
+        let i = 0; i < inputString.length; i++) {
+        !charMap[inputString[i]] ? charMap[inputString[i]] = 1 : charMap[inputString[i]]++;
+    }
+
+    /* Check through each character in thr map object. In order to be arranged into a palindrome,
+      only one set of characters can be odd (the center letter).
+    */
+    let oddsLeft = 1;
+    for (let letter in charMap) {
+        if (charMap[letter] % 2 !== 0) {
+            oddsLeft--;
+        }
+    }
+
+    //if there is 0-1 odd set of characters, we can make it into a palindrome
+    if (oddsLeft > -1) return true;
+
+    return false;
 }
 
 console.log(palindromeRearranging('aabb'));
