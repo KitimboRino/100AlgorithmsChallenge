8559584f111b27156a797702e99bf0c6dae6cf48
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-09-14 18:03:48 +0300

    Algo(seatsInTheater.ts): solve seatsInTheater

--- reverseAString/reverseAString.ts
+++ reverseAString/reverseAString.ts
@@ -1,6 +1,25 @@
-export function reverseAString(str: string): string {
+/*
+Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.
+*/
 
+export function reverseAString(str: string): string {
+    //Create pointers
+    let left = 0; 
+    let right = str.length - 1;
+    
+    //Using the beginning and end pointers, iterate down to the center of the array that represents the string, and swap values.
+    while(left <= right) {
+        //Swap the values.
+        [str[left], str[right]] = [str[right], str[left]];
+        
+        //Update the pointer values.
+        left++;
+        right--;
+    }
+    
+    return str;
 }
 
-// console.log(reverseAString('hello'));
-// console.log(reverseAString('Howdy'));
+console.log(reverseAString('hello'));
+console.log(reverseAString('Howdy'));
--- seatsInTheater/seatsInTheater.ts
+++ seatsInTheater/seatsInTheater.ts
@@ -1,5 +1,6 @@
 export function seatsInTheater(nCols: number, nRows: number, col: number, row: number): number {
-
+ //Since we are including this column in the area, add it back into the equation.
+ return (nCols - col + 1) * (nRows - row);
 }
 
-// console.log(seatsInTheater(16, 11, 5, 3));
+console.log(seatsInTheater(16, 11, 5, 3));
