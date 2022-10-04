bea4de13621e0ae50e9ebd5a7c0ca635e63d7cb7
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-10-04 09:57:32 +0300

    algo(sortByHeights.ts): Solved sortByHeights algorithm

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,22 @@
 export function sortByHeight(a: number[]): number[] {
+    // Extract non -1 values
+    const b: number[] = a.filter((element) => {
+        if (element !== -1) {
+            return element;
+        }
+    }).sort((a,b) => a-b);
 
+    let inedexx: number = 0;
+
+  // Loop through array, placing sorted values into place.
+    for (let i: number=0; i<a.length; i++) {
+        if (a[i] !== -1) {
+            a[i] = b[inedexx];
+            inedexx++;
+        }
+    }
+
+    return a;
 }
 
-// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
