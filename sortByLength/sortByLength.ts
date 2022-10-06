afbcff5894d4fa4a4dc9158352688613b84230c3
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-10-06 08:02:55 +0300

    algo(sortBy.ts): Solved sortByHeights algorithm

--- sortByLength/sortByLength.ts
+++ sortByLength/sortByLength.ts
@@ -1,9 +1,10 @@
 export function sortByLength(inputArray: string[]): string[] {
-
+    
+    return inputArray.sort((a1: string, a2: string) =>a1.length - a2.length)
 }
 
-// console.log(sortByLength(["abc",
-// "",
-// "aaa",
-// "a",
-// "zz"]));
+console.log(sortByLength(["abc",
+"",
+"aaa",
+"a",
+"zz"]));
