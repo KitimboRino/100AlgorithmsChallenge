c6a6e33a3dca35f1e4a9f96e1c3339dc3e23e35f
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-10-10 08:31:21 +0300

    refact(stolenLunch.ts): Reviewed stolenLunch algorithm

--- stolenLunch/stolenLunch.ts
+++ stolenLunch/stolenLunch.ts
@@ -1,14 +1,24 @@
 export function stolenLunch(note: string): string {
-    const b: string[] = note.split(' ');
-    const dict:object= {
-        'a': '0', 'b': '1', 'c': '2', 'd': '3', 'e': '4', 'f': '5', 'g': '6', 'h': '7', 'i': '8', 'j': '9','0': 'a', '1': 'b', '2': 'c', '3': 'd', '4': 'e', '5': 'f', '6': 'g', '7': 'h', '8': 'i', '9': 'j', 
-    }
-    let retChar: string ='';
 
-    for (let char of b){
-        retChar += dict.hasOwnProperty(char) ? dict[char]:char;
+    /* Splitting the string into an array of characters. */
+    const b: string[] = note.split('');
+   
+    /* A dictionary that maps the letters to numbers and vice versa. */
+    const dict: object = {
+        'a': '0', 'b': '1', 'c': '2', 'd': '3', 'e': '4', 'f': '5', 'g': '6', 'h': '7', 'i': '8', 'j': '9', 
+        '0': 'a', '1': 'b', '2': 'c', '3': 'd', '4': 'e', '5': 'f', '6': 'g', '7': 'h', '8': 'i', '9': 'j', 
     }
+    let retChar: string = '';
+
+    /* Iterating through the array and checking if the dictionary has the key. If it does, it will return
+    the value of the key. If it doesn't, it will return the character. */
+    for(let char of b) {
+        retChar += dict.hasOwnProperty(char) ? dict[char] : char;
+    }
+
+    /* Returning the value of the variable `retChar`. */
     return retChar;
+
 }
 
 console.log(stolenLunch("you'll n4v4r 6u4ss 8t: cdja"));
--- stringsConstruction/stringsConstruction.ts
+++ stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,30 @@
 export function stringsConstruction(a: string, b: string): number {
+    const aCount: object = getAlphabetCountts(a);
+    const bCount: object = getAlphabetCountts(b);
+    const counts: number[] = [];
+    for (let char in aCount) {
+        if(bCount.hasOwnProperty(char)) {
+            counts.push(Math.floor(bCount[char] / aCount[char]));
+        } else {
+            return 0;
+        }
+    }
+
+    return Math.min(...counts);
 
 }
 
-// console.log(stringsConstruction('abc', 'abccba'));
+function getAlphabetCountts(word: string): object {
+    const chars: string[] = word.split('');
+    const alphabetCount: object = {};
+
+    chars.forEach(char => {
+        if(alphabetCount.hasOwnProperty(char)) {
+            alphabetCount[char]++;
+        } else {
+            alphabetCount[char] = 1;
+        }
+    });
+    return alphabetCount;
+}
+console.log(stringsConstruction('abc', 'abccba'));
