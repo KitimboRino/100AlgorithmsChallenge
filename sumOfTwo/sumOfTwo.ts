31a3d854c4ea94925dae34f0b9b63e315e924e31
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-10-17 09:25:46 +0300

    algo(sumOfTwo.ts): solved sumOfTwo algorithm

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,14 @@
-export function sumOfTwo(a: number[], b: number[], v: number): boolean {
-
+export function sumOfTwo(a: number[], b: number[], c: number): boolean {
+    
+    for (let i: number = 0; i < a.length; i++) {
+        
+        for (let j: number = 0; j < b.length; j++) {
+            if (a[i] + b[j] === c) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-// console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
