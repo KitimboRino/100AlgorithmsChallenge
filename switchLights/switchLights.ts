fff84ce288132e81284767ec509598225db4d329
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-10-18 08:45:05 +0300

    algo(switchLights.ts): solved switchLights algorithm

--- switchLights/switchLights.ts
+++ switchLights/switchLights.ts
@@ -1,6 +1,16 @@
 export function switchLights(a: number[]): number[] {
-
+    
+    const lights: number[] = a;
+    
+    for(let i: number = 0; i < a.length; i++) {
+        if(a[i] !== 0){
+            for(let j: number = i; j >= 0; j--){
+                lights[j] = lights[j] === 0 ? 1 : 0;
+            }
+        } 
+    }
+    return lights
 }
 
-// console.log(switchLights([1, 1, 1, 1, 1]));
-// console.log(switchLights([0, 0]));
+console.log(switchLights([1, 1, 1, 1, 1]));
+console.log(switchLights([0, 0]));
