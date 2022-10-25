7af6a43bfce7f00bbd701c00026291904fd01911
Rino Kitimbo <kitimborinoemma0580@gmail.com>
2022-10-25 08:53:59 +0300

    algo(validTime.ts): solved validTime.ts algorithm

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,11 @@
 export function validTime(time: string): boolean {
+    let hour: string = time.substring(0, time.lastIndexOf(':'));
 
+    let minutes: string = time.substring(time.lastIndexOf(':') + 1);
+
+    return (parseInt(hour) <= 24 && parseInt(hour) >= 0) && (parseInt(minutes) < 60 && parseInt(minutes) >= 0)
 }
 
-// console.log(validTime('13:58'));
-// console.log(validTime('25:51'));
-// console.log(validTime('02:76'));
+console.log(validTime('13:58'));
+console.log(validTime('25:51'));
+console.log(validTime('02:76'));
